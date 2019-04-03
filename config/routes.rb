1bfab882c7ce363cbdaa0b49878f1e2c10bbde28
aaa <aaa@iMac-2.local>
2019-04-03 17:09:59 +0800

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   get 'user/index'
   get 'user/show'
   get 'user/new'
+  root 'user#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
